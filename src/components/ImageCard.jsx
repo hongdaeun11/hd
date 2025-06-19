844c295866e6b93e71153d13057061caebe57884
hongdaeun11 <sally20114@gmail.com>
2025-06-19 11:48:34 +0900

    Update ImageCard.jsx

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -1,5 +1,5 @@
 function ImageCard({imagesrc}){
-    const url = "http://localhost:5175"+imagesrc
+    const url = "http://localhost:5173"+imagesrc
     return (
         <>
             <img src={url} style={{width:"200px", height:"200px"}}/>
